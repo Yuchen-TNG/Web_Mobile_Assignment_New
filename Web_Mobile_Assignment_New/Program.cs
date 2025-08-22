0ffeafe3980f6e1d799db0c7f3d4ac6fb8d5f38a
Yuchen-TNG <tngyc-wm24@student.tarc.edu.my>
2025-08-22 23:27:32 +0800

    a

--- Web_Mobile_Assignment_New/Program.cs
+++ Web_Mobile_Assignment_New/Program.cs
@@ -1,5 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 
 app.MapGet("/", () => "Hello World!");
 

fcfe37aa0b331d946c5762dbc63d96ec7ff89836
Yuchen-TNG <tngyc-wm24@student.tarc.edu.my>
2025-08-24 17:27:10 +0800

    super modify

--- Web_Mobile_Assignment_New/Controllers/HomeController.cs
+++ Web_Mobile_Assignment_New/Controllers/HomeController.cs
@@ -1,11 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Web_Mobile_Assignment_New.Models;
 
-namespace Web_Mobile_Assignment_New.Controllers;
-
-public class HomeController : Controller
+namespace Web_Mobile_Assignment_New.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
+        private readonly DB _context;
+
+        public HomeController(DB context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            Console.WriteLine(_context.Database.GetDbConnection().ConnectionString);
+            var houses = _context.Houses.ToList(); // 拿所有房子
+            return View(houses);
+        }
+
+        [HttpGet]
+        public IActionResult AddHouse()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddHouse(House house)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Houses.Add(house);
+                _context.SaveChanges();
+                return RedirectToAction("Index"); // 添加后跳去主页面
+            }
+            return View(house);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var house = _context.Houses.FirstOrDefault(h => h.Id == id);
+            if (house == null) return NotFound();
+            return View(house);
+        }
     }
 }
---
+++ Web_Mobile_Assignment_New/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Web_Mobile_Assignment_New.Models;
+
+
+public class DB : DbContext
+{
+    public DB(DbContextOptions<DB> options) : base(options) { }
+
+    public DbSet<House> Houses { get; set; }
+}

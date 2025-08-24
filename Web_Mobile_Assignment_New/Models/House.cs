e584e76f39218ce417bc5f7225769558d007bdde
Leonchang06 <leoncrh-wm24@student.tarc.edu.my>
2025-08-24 18:40:12 +0800

    aaa

--- Web_Mobile_Assignment_New/Models/House.cs
+++ Web_Mobile_Assignment_New/Models/House.cs
@@ -1,10 +1,12 @@
-﻿namespace Web_Mobile_Assignment_New.Models;
-
-public class House
+﻿namespace Web_Mobile_Assignment_New.Models
 {
-    public int Id { get; set; } // 主键
-    public string Title { get; set; } = "";
-    public string Address { get; set; } = "";
-    public decimal Price { get; set; }
-    public string Description { get; set; } = "";
+    public class House
+    {
+        public int Id { get; set; } // 主键
+        public string Title { get; set; } = "";
+        public string Address { get; set; } = "";
+        public decimal Price { get; set; }
+        public string Description { get; set; } = "";
+    }
+
 }
